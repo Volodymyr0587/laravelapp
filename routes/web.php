10ffcef8709233acfc445f213080b3617a34e7b8
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-08 22:37:45 +0300

    06 - Config & Env Variables

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $name = "Check Views";
-    return view('welcome', [
-        'name' => $name
-    ]);
+    return view('welcome');
 });
