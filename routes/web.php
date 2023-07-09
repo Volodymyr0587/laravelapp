d1f95fa8476c85ba7e543ee0ffc0b9319cb5aa83
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-09 15:25:54 +0300

    07 - Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/blog', [PostsController::class, 'index']);
+
+Route::resource('blog', PostsController::class);
