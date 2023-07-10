184b3f7cefdc6df48990c7b89e0d1df7eb492cf2
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-10 18:16:08 +0300

    08 - Single Action Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostsController;
 use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Support\Facades\Route;
@@ -18,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/blog', [PostsController::class, 'index']);
 
 Route::resource('blog', PostsController::class);
+
+// Route for invoke method
+Route::get('/', HomeController::class);
