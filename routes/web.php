120a7b61993c2ef8504db3e5c7c529be13b4ad07
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-11 16:21:32 +0300

    13 - How to Use Route Prefixes

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,20 @@ use Illuminate\Support\Facades\Route;
     OPTIONS - Ask the server which verbs are allowed
 */
 
-// GET
-Route::get('/blog', [PostsController::class, 'index'])->name('blog.index');
-// GET  single post
-Route::get('/blog/{id}', [PostsController::class, 'show'])->name('blog.show');
-
-// POST
-Route::get('/blog/create', [PostsController::class, 'create'])->name('blog.create');
-Route::post('/blog', [PostsController::class, 'store'])->name('blog.store');
-
-// PUT OR PATCH
-Route::get('/blog/edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
-Route::patch('/blog/{id}', [PostsController::class, 'update'])->name('blog.update');
-
-// DELETE
-Route::delete('/blog/{id}', [PostsController::class, 'destroy'])->name('blog.destroy');
-
+Route::prefix('/blog')->group(function () {
+    // GET
+    Route::get('/', [PostsController::class, 'index'])->name('blog.index');
+    // GET  single post
+    Route::get('/{id}', [PostsController::class, 'show'])->name('blog.show');
+    // POST
+    Route::get('/create', [PostsController::class, 'create'])->name('blog.create');
+    Route::post('/', [PostsController::class, 'store'])->name('blog.store');
+    // PUT OR PATCH
+    Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
+    Route::patch('/{id}', [PostsController::class, 'update'])->name('blog.update');
+    // DELETE
+    Route::delete('/{id}', [PostsController::class, 'destroy'])->name('blog.destroy');
+});
 // Route::resource('blog', PostsController::class);
 
 // Route for invoke method
