d11e1b7fc9c54e183b88c62b067eed9b82425be5
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-12 17:24:49 +0300

    21 - Blade Variables & Control Structures (@if, @elseif, @else, @unless, @foresle, Loop Variable, etc)

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,5 +1,18 @@
-<h1>
-    Index method of Blogs
-</h1>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
 
-<a href={{ route('blog.show', ['id' => 1]) }}>Blog</a>
+    @forelse ($posts as $post)
+        {{ $loop->parent }}
+    @empty
+        <p>No posts have been set</p>
+    @endforelse
+
+</body>
+</html>
