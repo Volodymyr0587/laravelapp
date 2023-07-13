80d92f6e3e1f6947361649b2112c87e487d1ddc7
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-13 16:04:32 +0300

    25 - Insert Data using Eloquent

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('/blog')->group(function () {
+    Route::get('/create', [PostsController::class, 'create'])->name('blog.create');
     // GET
     Route::get('/', [PostsController::class, 'index'])->name('blog.index');
     // GET  single post
     Route::get('/{id}', [PostsController::class, 'show'])->name('blog.show');
     // POST
-    Route::get('/create', [PostsController::class, 'create'])->name('blog.create');
     Route::post('/', [PostsController::class, 'store'])->name('blog.store');
     // PUT OR PATCH
     Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
